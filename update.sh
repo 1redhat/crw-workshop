3cd654f968c1310272ab7ab0b5a35ae3179026fb
Craig Iverson <civerson1@gmail.com>
2020-04-29 00:02:49 -0700

    initail readme and update script

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 oc delete all -l app=guide
-#oc new-app --name=guide osevg/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/RedHatWorkshops/openshiftv4-workshop/ocp4.2" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/RedHatWorkshops/openshiftv4-workshop/ocp4.2/_module_groups.yml"
-oc new-app --name=guide osevg/workshopper CONTENT_URL_PREFIX="https://raw.githubusercontent.com/1redhat/crw-workshop/master" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/1redhat/crw-workshop/master/_groups_modules.yml"
+oc new-app quay.io/osevg/workshopper --name=guide \
+      -e WORKSHOPS_URLS="https://raw.githubusercontent.com/1redhat/crw-workshop/master/_crw_workshop.yml" \
+      -e JAVA_APP=false 
 oc expose svc/guide
+oc status

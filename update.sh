a6d89339843e0a690c7cf3002d452d67e821f662
Craig Iverson <civerson1@gmail.com>
2020-04-30 11:51:37 -0700

    add creating project and changing project to update script

--- update.sh
+++ update.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+oc new-project workshop
+oc project workshop
 
 oc delete all -l app=guide
 oc new-app quay.io/osevg/workshopper --name=guide \
       -e WORKSHOPS_URLS="https://raw.githubusercontent.com/1redhat/crw-workshop/master/_crw_workshop.yml" \
       -e JAVA_APP=false 
 oc expose svc/guide
+
 oc status
